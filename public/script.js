483b657131e4d1ef67ffa49a5fc4ef04e67c9f24
strangematter2011 <strangematter2011@gmail.com>
2026-03-07 18:12:20 +0000

    nice idea by gpt to be fair as much as i slander it in my update notes

--- public/script.js
+++ public/script.js
@@ -1,3 +1,24 @@
+const card = document.getElementById("card");
+
+card.addEventListener("mousemove", (e) => {
+
+  const rect = card.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  const centerX = rect.width / 2;
+  const centerY = rect.height / 2;
+
+  const rotateX = -(y - centerY) / 10;
+  const rotateY = (x - centerX) / 10;
+
+  card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+});
+
+card.addEventListener("mouseleave", () => {
+  card.style.transform = "rotateX(0) rotateY(0)";
+});
+
 let btnEL = document.querySelector("button");
 let headingEL = document.querySelector("h2");
 let nameInput = document.querySelector("#nameInput");
